94c9685f31ae654314dbd3639b7fa951a9f4d628
daddabarba <davidebarbieri97@gmail.com>
2020-10-12 18:36:15 +0200

    Made psi a class to have memory

--- losses/actors.py
+++ losses/actors.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
 
-def makePolicyLoss(psi):
+def makePolicyLoss(Psi):
 
     def loss():
         def loss(policy, episode, discount_factor):
@@ -21,14 +21,14 @@ def makePolicyLoss(psi):
             # YOUR CODE HERE
 
             # −∑𝑡log𝜋𝜃(𝑎𝑡|𝑠𝑡)𝐺𝑡
-            states, actions, rewards, _ = episode
-            log_probs = torch.log(policy.get_probs(states,actions))
-            rtrn = 0
+            states, actions, _, _ = episode
+            psi = Psi(policy=policy, episode=episode, discount_factor=discount_factor)
+
+            log_probs = torch.log(policy.get_probs(states, actions))
             loss = 0
 
             for t in range(len(states) - 1, -1, -1):
-                loss += log_probs[t] * psi(t, rewards, rtrn, discount_factor)
-                rtrn = discount_factor * rtrn + rewards[t]
+                loss += log_probs[t] * next(psi)
 
             return -loss
 
@@ -37,5 +37,23 @@ def makePolicyLoss(psi):
     return loss
 
 @makePolicyLoss
-def REINFORCE(t, rewards, rtrn, discount_factor):
-    return rewards[t] + discount_factor * rtrn
+class REINFORCE:
+    def __init__(self, policy, episode, discount_factor):
+
+        rewards = episode[2]
+
+        self._rewards = rewards
+        self._gamma = discount_factor
+
+        self._t = len(rewards)
+        self._G = 0
+
+    def __next__(self):
+
+        self._t -= 1
+
+        if self._t < 0:
+            return 0
+
+        self._G = self._rewards[self._t] + self._gamma * self._G
+        return self._G
